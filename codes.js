ccb19bac591116ace96c38350c252ec4ca6fdc52
Martin Leopold <m@martinleopold.com>
2018-06-20 12:45:53 +0200

    Permutations with inverse

--- codes.js
+++ codes.js
@@ -53,8 +53,64 @@ function combinations(n, p, offset = 0) {
   return out;
 }
 
+// Create an array of sequential integers starting at 0
+function seq(length) {
+  let out = [];
+  for (let i=0; i<length; i++) {
+    out.push(i);
+  }
+  return out;
+}
+
+// Shuffle an array (mutating)
+function shuffle(array, salt = '') {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  if (salt) {
+    Math.seedrandom(salt);
+  }
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+// Permutation of sequential integers with inverse
+function perm(length, salt = '') {
+  let forward = shuffle(seq(length), salt);
+  let inverse = forward.reduce( (acc, val, idx) => {
+    acc[val] = idx;
+    return acc;
+  }, []);
+  return { forward, inverse };
+}
+
+// Construct permutation function with inverse
+class Perm {
+  constructor(length, salt = '') {
+    this.data = perm(length, salt);
+  }
+  get(idx) {
+    return this.data.forward[idx];
+  }
+  inv(idx) {
+    return this.data.inverse[idx];
+  }
+}
+
 (function main() {
   let c = combinations(3, 10);
   console.log(c);
   console.log(c.length);
+  
+  let p = new Perm(10, 'salt')
+  console.log(p);
+  console.log(p.get(0), p.inv(7));
 })();
