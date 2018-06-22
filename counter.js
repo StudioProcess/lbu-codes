347f7757ac2abf7186a592e3c68c26e21e19cc6d
Martin Leopold <m@martinleopold.com>
2018-06-22 13:39:22 +0200

    Aligned counter with reference

--- counter.js
+++ counter.js
@@ -1,26 +1,31 @@
-
 /* 
   http://bit.ly/2JsmYeU 
+  
   <span 
     class="population-clock" 
     data-start-value="7550262101" 
     data-date="2017-07-01" 
     data-per-second="2.62">
   </span>
-
 */
 
 const startDate = '2017-07-01';
 const startValue = 7550262101;
 const perSecond = 2.62;
 
-/* TODO: difference to reference:
-  they: 7.630.729.260
-  me:   7,630,710,462
-*/
+// My original calculation
+// export function current() {
+//   let start = new Date(startDate);
+//   return startValue + (Date.now() - start.getTime()) / 1000 * perSecond;
+// }
+
+// Calculation from http://bit.ly/2JsmYeU 
+// The display there is wrong since it seems to ever only increment by 3 (instead of 2.62)
 export function current() {
   let start = new Date(startDate);
-  return startValue + (Date.now() - start.getTime()) / 1000 * perSecond;
+  start.setHours(0); // Removes time zone difference (in my case 2)
+  let diff = Math.round((new Date() - start)/1000) + 1;
+  return Math.round(diff * perSecond + startValue);
 }
 
 export function currentFormatted(locale = 'en') {
