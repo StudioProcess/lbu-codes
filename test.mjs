db3893fb496d58a7554f7fcf2745662df335d5b0
Martin Leopold <m@martinleopold.com>
2018-06-25 15:15:34 +0200

    Added test cases

--- test.mjs
+++ test.mjs
@@ -1,10 +1,79 @@
 import { Codes } from './codes.mjs';
 import test from 'tape';
+import 'seedrandom';
 
+
+// Shuffle an array (mutating)
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
 
 function testCodes(codes) {
-  test('Check decoding', t => {
-    t.skip();
+  test('Check length of codes', t => {
+    for (let c of codes) {
+      t.equals(c.length, codes.p);
+    }
+    t.end();
+  });
+  
+  test('Check range of code points', t => {
+    for (let c of codes) {
+      for (let cp of c) {
+        t.assert(cp >= 0 && cp < codes.n);
+      }
+    }
+    t.end();
+  });
+  
+  test('Check decoding (method 1)', t => {
+    Array.from(codes).forEach( (c, i) => {
+      t.equals( codes.decode(c), i );
+    });
+    t.end();
+  });
+  
+  test('Check decoding (method 2)', t => {
+    for (let i=0; i<codes.length; i++) {
+      let c = codes.encode(i);
+      t.equals( codes.decode(c), i );
+    }
+    t.end();
+  });
+  
+  test('Check decoding (method 3, argument list instead of array)', t => {
+    for (let i=0; i<codes.length; i++) {
+      let c = codes.encode(i);
+      t.equals( codes.decode.apply(codes, c), i );
+    }
+    t.end();
+  });
+  
+  test('Check decoding with reversed code', t => {
+    for (let i=0; i<codes.length; i++) {
+      let c = codes.encode(i).reverse();
+      t.equals( codes.decode(c), i );
+    }
+    t.end();
+  });
+  
+  test('Check decoding with shuffled code', t => {
+    Math.seedrandom('seed');
+    for (let i=0; i<codes.length; i++) {
+      let c = shuffle( codes.encode(i) );
+      t.equals( codes.decode(c), i );
+    }
     t.end();
   });
 }
@@ -13,8 +82,4 @@ function testCodes(codes) {
 (function main() {
   let codes = new Codes(9, 4, 'salt');
   testCodes(codes);
-  
-  // for (let c of codes) {
-  //   console.log(c.length);
-  // }
 })();
